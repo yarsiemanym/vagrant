1c63bee479eb4c874e20821dfcd65695113fe08c
Ray Saltrelli <raymond.saltrelli@protonmail.com>
2019-10-26 19:52:21 -0400

    Add optional `mode` parameter

--- korriban/shell/install.ps1
+++ korriban/shell/install.ps1
@@ -1,18 +1,28 @@
+param([String]$mode='host')
+
 # Setup Boxstarter and Chocolatey
-. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex
-get-boxstarter -Force
+. { Invoke-WebRequest -useb http://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression
+Get-Boxstarter -Force
 
 # Enter Boxstarter shell
 C:\ProgramData\Boxstarter\BoxstarterShell.ps1
 
 # Configure Windows using Boxstarter commands
 Install-WindowsUpdate
-Disable-UAC
-Update-ExecutionPolicy
 Set-WindowsExplorerOptions -EnableShowFullPathInTitleBar -EnableShowFileExtensions
 
 # Install packages using Chocolatey commands
-chocolatey feature enable -n=allowGlobalConfirmation
+choco feature enable -n=allowGlobalConfirmation
+
+if ($mode -ne 'virtual')
+{
+    choco install Zoom
+    choco install Slack
+    choco install Docker-Desktop
+    choco install ShrewSoftVPN
+    choco install OpenVPN
+}
+
 choco install IIS-ApplicationDevelopment -source windowsfeatures
 choco install IIS-ASPNET -source windowsfeatures
 choco install IIS-ASPNET45 -source windowsfeatures
@@ -32,11 +42,17 @@ choco install URLRewrite
 choco install Firefox
 choco install GitKraken
 choco install Git
+choco install SQL-Server-Management-Studio
 choco install VisualStudio2017Professional
 choco install VisualStudio2017-Workload-NetWeb
 choco install VisualStudio2017-Workload-ManagedDesktop
+choco install NodeJS.Install
 choco install DotNet3.5 # Why is this needed?
-choco install dotnetcore-sdk 
-choco install vscode
-choco install nuget.commandline 
-chocolatey feature disable -n=allowGlobalConfirmation
+choco install DotNet4.6.2
+choco install NetFx-4.6.2-DevPack
+choco install DotNetCore-SDK 
+choco install VSCode
+choco install NuGet.CommandLine 
+choco install Postman
+
+choco feature disable -n=allowGlobalConfirmation
